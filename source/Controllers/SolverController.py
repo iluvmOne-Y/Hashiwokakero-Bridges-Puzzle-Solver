44d7b175f1c01fffa653cc30b707e86073d176c8
Livens <dangkhoah77@gmail.com>
2025-03-26 17:48:09 +0700

    [feat] add SolverController for managing solver functions and solving levels

---
+++ source/Controllers/SolverController.py
@@ -0,0 +1,38 @@
+import Data.data_Types as _Types
+
+from typing import Callable
+
+from Solvers import PySAT  # , AStar, Backtracking, BruteForce
+
+SolverFunction = Callable[[_Types.Level], _Types.Matrix | None]
+
+# Define the solvers
+solvers: dict[str, SolverFunction] = {
+    "PySAT": PySAT.Solve,
+    # "AStar": AStar,
+    # "Backtracking": Backtracking,
+    # "BruteForce": BruteForce,
+}
+
+
+def Solve(level: _Types.Level, solver: str = "PySAT") -> _Types.Matrix | None:
+    """Solve the level.
+
+    ### Parameters:
+    - level: The level to solve.
+    - solver: The solver to use. Possible values are
+        + "PySAT"
+        + "AStar"
+        + "Backtracking"
+        + "BruteForce".
+
+    ### Returns:
+    - The solution matrix or `None` if no solution is found.
+    """
+    if solver not in solvers:
+        raise ValueError(f"Solver '{solver}' is not available.")
+
+    return solvers[solver](level)
+
+
+__all__ = ["Solve"]
