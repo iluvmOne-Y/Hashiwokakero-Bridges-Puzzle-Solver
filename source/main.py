978524871dd39b441a2720631d55c856085cde86
Livens <dangkhoah77@gmail.com>
2025-03-26 17:48:23 +0700

    [feat] add main entry point for level parsing and solution writing

---
+++ source/main.py
@@ -0,0 +1,20 @@
+import Data.data_Types as _Types
+
+import Controllers.FileController as FileController
+import Controllers.SolverController as SolverController
+
+if __name__ == "__main__":
+    # Parse the level from the input file
+    level: _Types.Level = FileController.ParseLevelFromFile(1)
+
+    # Generate the edges of the level
+    if level:
+        solution: _Types.Matrix = SolverController.Solve(level)
+    else:
+        solution = None
+
+    if solution:
+        level.grid = solution
+        FileController.WriteSolutionToFile(level)
+    else:
+        print("No solution found.")
